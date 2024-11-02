5c02ea539cb57fe3a352bca5d7191dd630d131d3
joaopespindola <joaopedro.espindola@outlook.com>
2024-11-02 09:00:11 -0300

    feat: config eslint, babel and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    parser: "@babel/eslint-parser",  
+    root: true,
+    extends: '@react-native-community',
+    plugins: ['react', 'react-hooks', 'import'],
+    settings: {
+      'import/resolver': {
+        'babel-plugin-root-import': {
+          rootPathSuffix: 'src',
+        },
+      },
+    },
+    rules: {
+      semi: [2, 'never'],
+      'no-extra-boolean-cast': 'off',
+    },
+  }
--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -1,2 +1,4 @@
 export * from './Text'
 export * from './Logo'
+export * from './Button'
+export * from './Input'
